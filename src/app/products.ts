2f5c56409c0704e4ecfa31fa01043a1b30c7f3f3
Atsushi Yoshinaga <a.yoshinaga0314@gmail.com>
2020-08-13 23:03:38 +0900

    詳細ページの作成

---
+++ src/app/products.ts
@@ -0,0 +1,43 @@
+export const products = [
+    {
+        image_src: './assets/img/social-media-5187243_640.png',
+        name: 'How to design your ideal life',
+        category: 'lifestyle',
+        author: 'James T',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText1: 'headingText1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText2: 'headingText2 headingText2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText3: 'headingText3 headingText3 headingText3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    },
+    {
+        image_src: './assets/img/mountain-5431950_640.png',
+        name: 'The Programing thinking',
+        category: 'Programing',
+        author: 'A Tsushi',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText1: 'headingText1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText2: 'headingText2 headingText2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText3: 'headingText3 headingText3 headingText3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    },
+    {
+        image_src:'./assets/img/social-media-5187243_640.png',
+        name: 'A way of consulting ',
+        category: 'Business',
+        author: 'Taro Yo',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText1: 'headingText1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText2: 'headingText2 headingText2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText3: 'headingText3 headingText3 headingText3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    },
+    {
+        image_src: './assets/img/mountain-5431950_640.png',
+        name: 'Set your life style',
+        category: 'lifestyle',
+        author: 'James T',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim venia',
+        headingText1: 'headingText1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText2: 'headingText2 headingText2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        headingText3: 'headingText3 headingText3 headingText3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    },
+
+]
